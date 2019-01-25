7e9714c1b2f79d956f07b5a6fe12446ca2b8520e
rbowen <rbowen@rcbowen.com>
2019-01-25 09:58:17 -0500

    default to current month

--- meeting_minutes.pl
+++ meeting_minutes.pl
@@ -1,15 +1,17 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use DateTime;
 
 use LWP::Simple qw(get);
-usage() unless $ARGV[0] && $ARGV[1];
+use Getopt::Long qw(GetOptions);
+my $help=0;
+GetOptions("help" => \$help) or help();
+help() if $help;
 
-# TODO use the current year, month, automatically.
-my $year = $ARGV[0];
-my $month = $ARGV[1];
-
-usage() unless $year =~ m/\d\d\d\d/;
+my $dt = DateTime->now( time_zone => 'local' );
+my $year = $ARGV[0] || $dt->year();
+my $month = $ARGV[1] || $dt->month_name();
 
 my $url = 'https://www.centos.org/minutes/' . $year . '/' . $month;
 my $html = get $url;
@@ -39,8 +41,9 @@ my @html=split(/\n/, $html);
     print "There appear to be no meeting minutes for that month. See $url to confirm.\n";
 }
 
-sub usage {
+sub help {
     print "Usage: ./$0 YEAR MONTH - eg, ./$0 2018 September\n";
+    print "Defaults to current month of none specified.\n";
     exit();
 }
 
