4a788ce5a3e9d7f8c2fd22d635f770481413c86b
rbowen <rbowen@rcbowen.com>
2020-01-02 14:58:14 -0500

    Update for changes to theme/html

--- scripts/meeting_minutes.pl
+++ scripts/meeting_minutes.pl
@@ -13,7 +13,8 @@ my $dt = DateTime->now( time_zone => 'local' );
 my $year = $ARGV[0] || $dt->year();
 my $month = $ARGV[1] || $dt->month_name();
 
-my $url = 'https://www.centos.org/minutes/' . $year . '/' . $month;
+my $url = 'https://centos.org/minutes/' . $year . '/' . $month . '/';
+# print "*** $url ***\n";
 my $html = get $url;
 
 if ($html) {
@@ -21,20 +22,20 @@ my @html=split(/\n/, $html);
     foreach my $line(@html) {
 
         # Looking for meeting minutes like centos-meeting.2018-09-04-13.01.html
-        next unless $line =~ m/centos-meeting\.$year-\d\d-\d\d-\d\d\.\d\d\.html/;
+        next unless $line =~ m/(centos-meeting|epel)\.$year-\d\d-\d\d-\d\d\.\d\d\.html/;
 
         $line =~ s/^.+href="//;
 
-        my ($filename, $date) = ( $line =~ m/^(.+?)".+?"right">(.+?) / );
+        my ($filename, $date) = ( $line =~ m/^(.+?)".+?"indexcollastmod">(.+?) / );
 
         # On $date, there are meeting minutes in $filename. What SIG was that?
-        my $minutesurl = $url . "/$filename";
+        my $minutesurl = $url . "$filename";
         my $minutes = get $minutesurl;
 
-        # print $minutes;
-        my ($title) = ( $minutes =~ m!<h1>#centos-meeting: (.*?)</h1>! );
+        # Get the meeting title from the minutes.
+        my ($title) = ( $minutes =~ m!<h1>#(?:centos-meeting|epel): (.*?)</h1>! );
         
-        print "Meeting minutes from the $title, $date - $minutesurl  #CentOS #SIG\n";
+        print "Meeting minutes - $title, $date - $minutesurl  #CentOS #SIG\n";
 
     }
 } else {
