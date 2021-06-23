41b0d52ef64f060f620b68e618d0bd2bcfde310e
rbowen <rbowen@rcbowen.com>
2021-06-23 14:43:39 -0400

    Do more of my work for me.

--- scripts/sig_reporting/reporting.pl
+++ scripts/sig_reporting/reporting.pl
@@ -5,17 +5,28 @@ use DateTime;
 
 my %reporters = (
     1 => [
-        'Core', 'Config Management',
-        'PaaS', 'Software Collections',
-        'Hyperscale',
+        [ 'Core',                 'sig-core@centosproject.org' ],
+        [ 'Config Management',    'sig-configmanagement@centosproject.org' ],
+        [ 'Software Collections', 'sig-sclo@centosproject.org' ],
+        [ 'Hyperscale',           'sig-hyperscale@centosproject.org' ],
     ],
 
-    2 => [ 'Alt Arch', 'Cloud', 'NFV', 'Promo', 'Storage', 'Messaging', ],
+    2 => [
+        [ 'Alt Arch',  'sig-altarch@centosproject.org' ],
+        [ 'Cloud',     'sig-cloud@centosproject.org' ],
+        [ 'NFV',       'sig-nfv@centosproject.org' ],
+        [ 'Promo',     'centos-promo@centos.org' ],
+        [ 'Storage',   'sig-storage@centosproject.org' ],
+        [ 'Messaging', 'sig-messaging@centosproject.org' ],
+    ],
 
     3 => [
-        'Artwork',        'Cloud Instance',
-        'OpsTools',       'Public CI',
-        'Virtualization', 'Infrastructure',
+        [ 'Artwork',        'sig-artwork@centosproject.org' ],
+        [ 'Cloud Instance', 'sig-cloudinstance@centosproject.org' ],
+        [ 'OpsTools',       'sig-opstools@centosproject.org' ],
+        [ 'Public CI',      'ENOSIG' ],
+        [ 'Virtualization', 'sig-virt@centosproject.org' ],
+        [ 'Infrastructure', 'sig-infra@centosproject.org' ],
     ]
 
 );
@@ -23,19 +34,24 @@ my %reporters = (
 my @sigs;
 
 # Who is reporting next month?
-# my $dt = DateTime->now( time_zone => 'local' )->add( months => 1 );
-my $dt = DateTime->now( time_zone => 'local' );
+# Weirdly, just doing month+1 can sometimes skip past a shorter month.
+# eg, March 31 + 1 month puts you in May, not April.
+my $today = DateTime->now( time_zone => 'local' );
+my $dt =
+  DateTime->new( day   => 1, 
+                 month => $today->month(), 
+                 year  => $today->year() ) -> add( months => 1 );
 
 my $month = $dt->month_name;
 print "Next month is $month.\n";
 
-my $group = ( ($dt->month() % 3) || 3 );
+my $group = ( ( $dt->month() % 3 ) || 3 );
 
 print "We expect reports from SIGs in Group " . $group . ":\n\n";
 
 foreach my $sig ( @{ $reporters{$group} } ) {
-    print "  * $sig\n";
-    push @sigs, $sig;
+    print "  * $sig->[0]\n";
+    push @sigs, $sig->[0];
 }
 
 print "\n";
@@ -43,18 +59,25 @@ print "\n";
 print "Send the following reminders to these SIGs\n";
 
 foreach my $sig ( @{ $reporters{$group} } ) {
+    my $name  = $sig->[0];
+    my $email = $sig->[1];
 
     print qq~
 --------------------------------------------------
 
-SUBJECT: CentOS $sig SIG quarterly report for $month newsletter
+SUBJECT: CentOS $name SIG quarterly report for $month newsletter
+To: $email
 
-Dear $sig SIG,
+Dear $name SIG,
 
 As documented in the wiki, we request that each SIG reports quarterly
 about the accomplishments of their SIG during the previous quarter.
 
-As you may be aware, your SIG - the $sig SIG - is on the
+(You are receiving this because you are listed in the $name
+SIG ACL on accounts.centos.org. If that is not accurate, you should take
+it up with your SIG lead. See https://accounts.centos.org/groups/)
+
+As you may be aware, your SIG - the $name SIG - is on the
 list for $month reports. See "Reporting" section towards the bottom of
 https://wiki.centos.org/SpecialInterestGroup
 
