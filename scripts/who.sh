aca78744016d59d8c4049e87c8f4f96a12c931ce
rbowen <rbowen@rcbowen.com>
2022-02-10 13:29:26 -0500

    Very crude approximation of *who* is doing those contributions.

--- scripts/who.sh
+++ scripts/who.sh
@@ -3,5 +3,5 @@
 # not reliable, as one person will invariably appear more than once in a
 # given change, but it's a good way to get a list to start with.)
 
-egrep '@' rss_updates.html | egrep ' GMT ' | sed 's/^.* GMT - //;' | sed 's/\&lt.*$//' | sort | uniq -c | sort -rn > who_contributed.txt
+egrep '@' rss_updates.html | egrep ' GMT ' | sed 's/^.* GMT - //;' | sed 's/\&gt.*$//' | sort | uniq -c | sort -rn > who_contributed.txt
 
